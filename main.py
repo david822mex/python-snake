79dfac7f7855f1d3e8d36c8e09faacfa59e82246
david822mex <61433648+david822mex@users.noreply.github.com>
2020-11-17 13:17:39 -0600

    1

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
- # hello123456
+# hello123456
+import pandas
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+gray = len(data[data["Primary Fur Color"] == "Gray"])
+print(gray)
